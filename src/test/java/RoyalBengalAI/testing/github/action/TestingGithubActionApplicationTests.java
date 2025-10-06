471f7921e18430ba184ef16ee5107f3dab9dc6de
faizunnur <faizunnur71@gmail.com>
2025-10-06 22:05:24 +0600

    created maven project

---
+++ src/test/java/RoyalBengalAI/testing/github/action/TestingGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package RoyalBengalAI.testing.github.action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingGithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
