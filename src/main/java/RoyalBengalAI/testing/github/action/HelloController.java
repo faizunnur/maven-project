70f83da1ce17bb8f06b9b76ed031553da499e025
faizunnur <faizunnur71@gmail.com>
2025-10-07 19:22:57 +0600

    print hello in web

--- src/main/java/RoyalBengalAI/testing/github/action/HelloController.java
+++ src/main/java/RoyalBengalAI/testing/github/action/HelloController.java
@@ -1,7 +1,13 @@
 package RoyalBengalAI.testing.github.action;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Correction 1: Added this annotation
 public class HelloController {
+
+    @GetMapping("/hello") // Correction 2: Moved this annotation to the method
     public String sayHello() {
-        return "Hello, CI/CD! The pipeline is working! ";
+        return "Hello, CI/CD! The pipeline is working! 🚀";
     }
 }
